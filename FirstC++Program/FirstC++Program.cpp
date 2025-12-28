dc0b9cdd917abcf875342ae60af6e634af5edccc
Abdul Hussein <abdul.chs21@gmail.com>
2025-12-28 01:45:18 -0600

    Initial commit

---
+++ FirstC++Program/FirstC++Program.cpp
@@ -0,0 +1,16 @@
+// FirstC++Program.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// First, let’s create a program that asks the user to enter an integer, 
+// waits for them to input an integer, then tells them what 2 times that number is.
+// The program should produce the following output (assume I entered 4 as input):
+
+#include <iostream>
+
+int main()
+{
+ 
+	
+  
+}
+
