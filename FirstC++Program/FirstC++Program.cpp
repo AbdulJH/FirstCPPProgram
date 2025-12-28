b454b3a28a8d8f52e45efdf0e805e348bef841e2
Abdul Hussein <abdul.chs21@gmail.com>
2025-12-28 02:12:16 -0600

    Created addAndSubtract method

--- FirstC++Program/FirstC++Program.cpp
+++ FirstC++Program/FirstC++Program.cpp
@@ -1,11 +1,31 @@
-// FirstC++Program.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <iostream>
 
+
+// Write a program that asks the user to enter a number, and then enter a second number. 
+// The program should tell the user what the result of adding and subtracting 
+// the two numbers is.
+
+void addAndSubtract()
+{
+	std::cout << "Enter an integer: ";
+
+	int firstUserInput{};
+	std::cin >> firstUserInput;
+
+	std::cout << "Enter another integer: ";
+
+	int secondUserInput{};
+	std::cin >> secondUserInput;
+
+	std::cout << firstUserInput << " + " << secondUserInput << " is " << firstUserInput + secondUserInput << ".\n";
+	std::cout << firstUserInput << " - " << secondUserInput << " is " << firstUserInput - secondUserInput << ".\n";
+}
+
+
+//
 // First, let’s create a program that asks the user to enter an integer, 
 // waits for them to input an integer, then tells them what 2 times that number is.
 // The program should produce the following output (assume I entered 4 as input):
-
-#include <iostream>
 
 int main()
 {
@@ -17,10 +37,10 @@ int main()
 
 	std::cout << "Double that number is: " << userInput * 2 << '\n';
 
-	std::cout << "Triple that number is: " << userInput * 3;
-	
+	std::cout << "Triple that number is: " << userInput * 3 << '\n';
+
+	addAndSubtract();
 
 
 	return 0;
 }
-
